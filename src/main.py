ea0b984f07458f081cfd19f718e2d731588d7330
Dan McVay <danielmcvaymedia@gmail.com>
2026-02-11 18:51:15 +0000

    Implemented check temp function that calls every 5 seconds.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
+# Satellite Monitoring System
+# Imports
 from microbit import *
 import music
 
+# Warning and Critical Tones for Speaker
 def warning_buzzer():
     for x in range(3):
         music.pitch(440, 200)
@@ -10,3 +13,18 @@ def critical_buzzer():
     for x in range(5):
         music.pitch(880, 50)
         sleep(50)
+
+def check_temp():
+    temp = temperature()
+    if temp < 10 or temp > 30:
+        critical_buzzer()
+        display.scroll("DANGER: {}C".format(temp)) # .format is used as a workaround cause fstrings and concat don't work
+    elif temp < 15 or temp >  28:
+        warning_buzzer()
+        display.scroll("WARNING: {}C".format(temp))
+    else:
+        display.scroll("STABLE: {}C".format(temp))
+
+while True:
+    check_temp()
+    sleep(5000)
