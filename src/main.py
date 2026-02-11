b32370c355e75abfed148554d9e517a6535c2ec1
Dan McVay <danielmcvaymedia@gmail.com>
2026-02-11 19:16:12 +0000

    Refactored temperature thresholds and buzzer parameters into named constants for improved maintainability.

--- src/main.py
+++ src/main.py
@@ -3,23 +3,37 @@
 from microbit import *
 import music
 
+# Configuration Constants
+WARNING_PITCH = 440
+WARNING_DURATION = 200
+WARNING_BEEPS = 3
+CRITICAL_PITCH = 880
+CRITICAL_DURATION = 50
+CRITICAL_BEEPS = 5
+
+TEMP_CRITICAL_LOW = 10
+TEMP_CRITICAL_HIGH = 30
+TEMP_WARNING_LOW = 15
+TEMP_WARNING_HIGH = 28
+CHECK_INTERVAL = 5000
+
 # Warning and Critical Tones for Speaker
 def warning_buzzer():
-    for x in range(3):
-        music.pitch(440, 200)
+    for x in range(WARNING_BEEPS):
+        music.pitch(WARNING_PITCH, WARNING_DURATION)
         sleep(100)
 
 def critical_buzzer():
-    for x in range(5):
-        music.pitch(880, 50)
+    for x in range(CRITICAL_BEEPS):
+        music.pitch(CRITICAL_PITCH, CRITICAL_DURATION)
         sleep(50)
 
 def check_temp():
     temp = temperature()
-    if temp < 10 or temp > 30:
+    if temp < TEMP_CRITICAL_LOW or temp > TEMP_CRITICAL_HIGH:
         critical_buzzer()
         display.scroll("DANGER: {}C".format(temp)) # .format is used as a workaround cause fstrings and concat don't work
-    elif temp < 15 or temp >  28:
+    elif temp < TEMP_WARNING_LOW or temp >  TEMP_WARNING_HIGH:
         warning_buzzer()
         display.scroll("WARNING: {}C".format(temp))
     else:
@@ -27,4 +41,4 @@ def check_temp():
 
 while True:
     check_temp()
-    sleep(5000)
+    sleep(CHECK_INTERVAL)
