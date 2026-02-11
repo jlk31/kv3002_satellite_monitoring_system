b363a3da1f00a6e1981e2f5099c5aaee6bc8b5a2
Dan McVay <danielmcvaymedia@gmail.com>
2026-02-11 18:30:20 +0000

    Implemented warning buzzer and critical buzzer functions

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-print("hello world")
+from microbit import *
+import music
+
+def warning_buzzer():
+    for x in range(3):
+        music.pitch(440, 200)
+        sleep(100)
+
+def critical_buzzer():
+    for x in range(5):
+        music.pitch(880, 50)
+        sleep(50)
